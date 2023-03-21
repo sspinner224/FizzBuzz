04fcbb1a795ceb92dcf32734b9abdff8b8899bb4
sspinner224 <sspinner@quinnipiac.edu>
2023-03-21 19:42:32 -0400

    added to git ignore

--- Main.java
+++ Main.java
@@ -8,7 +8,9 @@ public class Main {
     while(count<100) {
        count++;
     
-       if(count%3 == 0) {
+       if(count%3 == 0 && count%5 == 0) {
+        System.out.println("FizzBuzz");
+       } else if(count%3 == 0) {
         System.out.println("Fizz");
        } else if (count%5 == 0) {
         System.out.println("Buzz");
